c9610d955a9c0724d67afaa4e2ba0f46e88792c2
soniaymkvaa <sofiia.yamkova@ucu.edu.ua>
2022-12-09 12:01:06 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package decorator;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Document document = new SmartDocument("gs://oop-course/Geico-2021.png");
+//        document = TimedDocument(document);
+//        document = CashedDocument(document);
+        System.out.println(document.parse());
+
+
+    }
+}
